8c43f9a97b42c9871f4be6783c9d0191235c624d
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2022-11-06 14:16:10 -0400

    si

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3002
+const port = 3001
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require("qrcode-terminal");
 
